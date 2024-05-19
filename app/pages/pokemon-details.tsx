533765a0385c2812d6ad121ab5d76152ccc0a114
richardbonneau <richardbonneau91@gmail.com>
2024-05-19 13:40:15 -0400

    inprogress: show pokemon image

--- app/pages/pokemon-details.tsx
+++ app/pages/pokemon-details.tsx
@@ -1,34 +1,43 @@
-import { useLocalSearchParams  } from "expo-router"
+import { useLocalSearchParams } from "expo-router"
 import React from "react"
-import { ActivityIndicator, Text, StyleSheet } from "react-native"
+import { Image, Text, StyleSheet } from "react-native"
 import { PageContainer, PokemonCard, PokemonsFooter } from "../../src/components"
 import { useQuery } from "@tanstack/react-query"
 import { PokemonService } from "../../src/services"
 
+function getPokemonIdFromUrl(url: string | string[] | undefined): string | undefined {
+  console.log("url", url)
+  if (typeof url === 'string') {
+    const parts = url.split('/');
+    return parts[parts.length - 2];
+  }
+}
 
 export default function PokemonDetails() {
   const params = useLocalSearchParams()
 
-  const url = params.url as string
-  const pokemonId = url.substring(url.lastIndexOf('/') + 1)
+  const pokemonId = getPokemonIdFromUrl(params.url)
+
+  if (!pokemonId) {
+    return <PageContainer title="Error"><Text>Error: Invalid URL</Text></PageContainer>
+  }
+
   const { data, isLoading, error } = useQuery({
-    queryKey:['pokemonDetails', params.id],
+    queryKey: ['pokemonDetails', params.id],
     queryFn: () => PokemonService.getPokemonDetails(pokemonId)
   });
 
-
   if (isLoading) {
-    return <Text>Loading...</Text>;
+    return <PageContainer title="Error"><Text>Loading...</Text></PageContainer>;
   }
 
   if (error) {
-    return <Text>An error occurred: {error.message}</Text>;
+    return <PageContainer title="Error"><Text>An error occurred: {error.message}</Text></PageContainer>;
   }
-  console.log("details data",{url,pokemonId,data})
+
   return (
     <PageContainer title="Pokemon">
-      <Text>{params.id}</Text>
-      <Text>{params.url}</Text>
+      <Image source={{ uri: data.sprites.front_default }} style={styles.image} />
     </PageContainer>
   )
 }
@@ -37,5 +46,9 @@ export default function PokemonDetails() {
 const styles = StyleSheet.create({
   contentContainerStyle: {
     padding: 16,
+  },
+  image: {
+    width: 200,
+    height: 200,
   }
 })
